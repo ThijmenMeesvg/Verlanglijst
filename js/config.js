da101b00a6f2b9e73da1f639c0ed430daa153896
Thijmen Mees <ovnscouting+it@gmail.com>
2026-08-12 12:08:41 +0200

    Add v2 category and item configuration

---
+++ js/config.js
@@ -0,0 +1,25 @@
+export const CATEGORIES = [
+  { id: "books", name: "Boeken", order: 1 },
+  { id: "games", name: "Spelletjes", order: 2 },
+  { id: "music", name: "Muziek", order: 3 },
+  { id: "clothing", name: "Kleding & Accessoires", order: 4 },
+  { id: "home", name: "Uitzet", order: 5 },
+  { id: "outdoor", name: "Sport & Buiten", order: 6 },
+  { id: "other", name: "Overig", order: 7 }
+];
+
+export const ITEM_STATUSES = {
+  AVAILABLE: "available",
+  RESERVED: "reserved",
+  BOUGHT: "bought"
+};
+
+export const VISIBILITY = {
+  PUBLIC: "public",
+  PRIVATE: "private"
+};
+
+export const PRIVATE_LISTS = {
+  GIFTS: "gifts",
+  THIJMEN: "thijmen"
+};
